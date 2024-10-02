4376c4eae3ae5a7e43f992a6d67c5fcf02420d31
LilyOfTheVoid <lillianofthevoid@gmail.com>
2024-10-01 18:25:32 -0600

    Initial commit

---
+++ src/main/java/net/lilyofthevoid/nullbound/Nullbound.java
@@ -0,0 +1,16 @@
+package net.lilyofthevoid.nullbound;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Nullbound implements ModInitializer {
+	public static final String MOD_ID = "nullbound";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lilyofthevoid/nullbound/NullboundClient.java
@@ -0,0 +1,10 @@
+package net.lilyofthevoid.nullbound;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NullboundClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
